a548e309c700102016356c743244b4f1de337b92
MiltonEM <esteban@unicauca.edu.co>
2023-03-01 14:26:04 -0500

    Ejemplo de programacón en javascript

---
+++ JavaScript VS Code/archivojavascript.js
@@ -0,0 +1,35 @@
+//alert('hola archivo java scripg')
+
+//Ingresar datos
+TD = prompt('Tipo de documento: ',);
+Nombre = prompt('Nombres: ',);
+Apellidos = prompt('Apellidos: ',);
+Direccion = prompt('Dirección: ',);
+Telefono = prompt('Número Telefónico: ',);
+Edad = prompt('Edad: ',);
+Estatura = prompt('Estatura: ',);
+EstadoCivil = prompt('Estado Civil: ',);
+Hijos = prompt('Número de hijos: ',);
+FechaContrato = prompt('Fecha de Contratación (DD/MM/AAAA): ',);
+DiasLaborados = prompt('Dias Laborados: ',);
+Sueldo = prompt('Sueldo: ',);
+
+//Visualisar datos ingresados
+document.write(TD);
+document.write(Nombre);
+document.write(Apellidos);
+document.write(Direccion);
+document.write(Telefono);
+document.write(Edad);
+document.write(Estatura);
+document.write(EstadoCivil);
+document.write(Hijos);
+document.write(FechaContrato);
+document.write(DiasLaborados);
+document.write(Sueldo);
+
+
+
+
+
+
